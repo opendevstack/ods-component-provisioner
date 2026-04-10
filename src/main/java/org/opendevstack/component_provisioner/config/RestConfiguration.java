222f475d9e2ae667f7013d1fa13055238783c088
soriaoli <sergio.soria_olivares.ext@boehringer-ingelheim.com>
2026-04-10 09:20:03 +0200

    [hidden-parameters-validation] - Add RestTemplateLoggingInterceptor.

--- src/main/java/org/opendevstack/component_provisioner/config/RestConfiguration.java
+++ src/main/java/org/opendevstack/component_provisioner/config/RestConfiguration.java
@@ -3,12 +3,15 @@ package org.opendevstack.component_provisioner.config;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.opendevstack.component_provisioner.server.controllers.interceptors.RestTemplateLoggingInterceptor;
 import org.springframework.boot.convert.ApplicationConversionService;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.util.DefaultUriBuilderFactory;
@@ -17,6 +20,7 @@ import org.springframework.web.util.DefaultUriBuilderFactory;
 @Slf4j
 public class RestConfiguration implements WebMvcConfigurer {
 
+    /*
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
         var uriTemplateHandler = new DefaultUriBuilderFactory();
@@ -26,6 +30,30 @@ public class RestConfiguration implements WebMvcConfigurer {
                 .uriTemplateHandler(uriTemplateHandler)
                 .build();
     }
+     */
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        var uriTemplateHandler = new DefaultUriBuilderFactory();
+        uriTemplateHandler.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.NONE);
+
+        var restTemplate = restTemplateBuilder
+                .uriTemplateHandler(uriTemplateHandler)
+                .requestFactory(() ->
+                        new BufferingClientHttpRequestFactory(
+                                new SimpleClientHttpRequestFactory()
+                        )
+                )
+                .build();
+
+
+        restTemplate.getInterceptors().add(
+                new RestTemplateLoggingInterceptor()
+        );
+
+
+        return restTemplate;
+    }
 
     @Bean(name = "patchRestTemplate")
     public RestTemplate patchRestTemplate(RestTemplateBuilder builder) {
@@ -34,9 +62,15 @@ public class RestConfiguration implements WebMvcConfigurer {
         var requestFactory =
                 new HttpComponentsClientHttpRequestFactory(httpClient);
 
-        return builder
+        var patchRestTemplate = builder
                 .requestFactory(() -> requestFactory)
                 .build();
+
+        patchRestTemplate.getInterceptors().add(
+                new RestTemplateLoggingInterceptor()
+        );
+
+        return patchRestTemplate;
     }
 
     @Override
---
+++ src/main/java/org/opendevstack/component_provisioner/server/controllers/interceptors/RestTemplateLoggingInterceptor.java
@@ -0,0 +1,45 @@
+package org.opendevstack.component_provisioner.server.controllers.interceptors;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+public class RestTemplateLoggingInterceptor implements ClientHttpRequestInterceptor {
+
+    @Override
+    public ClientHttpResponse intercept(
+            HttpRequest request,
+            byte[] body,
+            ClientHttpRequestExecution execution
+    ) throws IOException {
+
+        logRequest(request, body);
+
+        ClientHttpResponse response = execution.execute(request, body);
+
+        return response;
+    }
+
+    private void logRequest(HttpRequest request, byte[] body) {
+        log.debug("===== REST TEMPLATE REQUEST =====");
+        log.debug("URI         : {}", request.getURI());
+        log.debug("Method      : {}", request.getMethod());
+
+        log.debug("Headers     :");
+        request.getHeaders().forEach((key, value) ->
+                log.debug("  {}: {}", key, value)
+        );
+
+        if (body.length > 0) {
+            log.debug("Request Body: {}", new String(body, StandardCharsets.UTF_8));
+        } else {
+            log.debug("Request Body: <empty>");
+        }
+    }
+}
