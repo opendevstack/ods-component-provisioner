b6eb1b85826f20311886289010da74b04eda3ebb
zxBCN Esperalta_Gata,Sergio (IT EDP) EXTERNAL <sergio.esperalta_gata.ext@boehringer-ingelheim.com>
2026-04-28 13:20:09 +0200

    Add tests for `ControllerExceptionHandler`, `CreateIncidentParameterMapper`, and `ComponentNotFoundException`

--- src/test/java/org/opendevstack/component_provisioner/server/services/ComponentCatalogServiceTest.java
+++ src/test/java/org/opendevstack/component_provisioner/server/services/ComponentCatalogServiceTest.java
@@ -15,6 +15,8 @@ import org.opendevstack.component_provisioner.client.component_catalog.v1.api.Pr
 import org.opendevstack.component_provisioner.client.component_catalog.v1.auth.HttpBearerAuth;
 import org.opendevstack.component_provisioner.client.component_catalog.v1.model.CatalogItem;
 import org.opendevstack.component_provisioner.client.component_catalog.v1.model.CatalogItemUserActionMessageDefinition;
+import org.opendevstack.component_provisioner.client.component_catalog.v1.model.ProjectComponentExtendedInfo;
+import org.opendevstack.component_provisioner.client.component_catalog.v1.model.ProjectComponentInfo;
 import org.opendevstack.component_provisioner.client.component_catalog.v1.model.ProvisioningStatusUpdateRequest;
 import org.opendevstack.component_provisioner.client.component_catalog.v1.model.ProvisioningStatusUpdateRequestParametersInner;
 import org.opendevstack.component_provisioner.config.ApplicationPropertiesConfiguration;
@@ -367,16 +369,38 @@ class ComponentCatalogServiceTest {
         HttpBearerAuth auth = mock(HttpBearerAuth.class);
         when(componentCatalogApiClient.getAuthentication("bearerAuth")).thenReturn(auth);
 
-        List<org.opendevstack.component_provisioner.client.component_catalog.v1.model.ProjectComponentInfo> expectedComponents = List.of();
+        List<ProjectComponentInfo> expectedComponents = List.of();
         when(projectComponentsApi.getProjectComponents(projectKey)).thenReturn(expectedComponents);
 
         // when
-        List<org.opendevstack.component_provisioner.client.component_catalog.v1.model.ProjectComponentInfo> result = componentCatalogService.getProjectComponents(projectKey, accessToken);
+        List<ProjectComponentInfo> result = componentCatalogService.getProjectComponents(projectKey, accessToken);
 
         // then
         assertThat(result).isSameAs(expectedComponents);
         verify(auth).setBearerToken(accessToken);
         verify(projectComponentsApi).getProjectComponents(projectKey);
+    }
+
+    @Test
+    void givenValidInput_whenGetProjectComponentExtendedInfoIsCalled_thenExtendedInfoIsReturned() {
+        // given
+        String projectKey = "PRJ-1";
+        String componentId = "CMP-1";
+        String accessToken = "access-token";
+
+        HttpBearerAuth auth = mock(HttpBearerAuth.class);
+        when(componentCatalogApiClient.getAuthentication("bearerAuth")).thenReturn(auth);
+
+        ProjectComponentExtendedInfo expectedInfo = new ProjectComponentExtendedInfo();
+        when(projectComponentsApi.getProjectComponentById(projectKey, componentId)).thenReturn(expectedInfo);
+
+        // when
+        ProjectComponentExtendedInfo result = componentCatalogService.getProjectComponentExtendedInfo(projectKey, componentId, accessToken);
+
+        // then
+        assertThat(result).isSameAs(expectedInfo);
+        verify(auth).setBearerToken(accessToken);
+        verify(projectComponentsApi).getProjectComponentById(projectKey, componentId);
     }
 
     @Test
