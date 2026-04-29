d0e2ec2a0ef547a121f0eba3717afe9887b42bb4
soriaoli <sergio.soria_olivares.ext@boehringer-ingelheim.com>
2026-04-29 15:49:48 +0200

    [provision-status-component] - Add unit tests.

---
+++ src/test/java/org/opendevstack/component_provisioner/server/model/ProjectComponentProvisionStatusMother.java
@@ -0,0 +1,8 @@
+package org.opendevstack.component_provisioner.server.model;
+
+public class ProjectComponentProvisionStatusMother {
+
+    public static ProjectComponentProvisionStatus of() {
+        return ProjectComponentProvisionStatus.builder().build();
+    }
+}
