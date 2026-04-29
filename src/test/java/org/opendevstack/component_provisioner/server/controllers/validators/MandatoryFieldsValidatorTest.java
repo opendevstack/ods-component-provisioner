20a6ea235efd6dff6cf3828c68eae56c39442ba7
Vila,Jordi (IT EDP) <jordi.vila@boehringer-ingelheim.com>
2026-04-29 09:04:13 +0200

    Increase test coverage

--- src/test/java/org/opendevstack/component_provisioner/server/controllers/validators/MandatoryFieldsValidatorTest.java
+++ src/test/java/org/opendevstack/component_provisioner/server/controllers/validators/MandatoryFieldsValidatorTest.java
@@ -129,4 +129,77 @@ class MandatoryFieldsValidatorTest {
         // then
         assertThat(actionParam.getValue()).isEqualTo(List.of("defaultValue"));
     }
+
+    @Test
+    void givenMandatoryParameterWithNullValue_whenValidate_thenExceptionIsThrown() {
+        // given
+        var bearerToken = "bearer-token";
+
+        CatalogItem catalogItem = CatalogItemMother.of();
+        CatalogItemUserActionParameter mandatoryParam = CatalogItemUserActionParameterMother.of(
+                "mandatoryParam",
+                "defaultValue"
+        );
+        catalogItem.getUserActions().getFirst().setParameters(List.of(mandatoryParam));
+
+        when(authenticationProvider.getAccessToken()).thenReturn(bearerToken);
+        when(componentCatalogService.getCatalogItem(any(), any(), any()))
+                .thenReturn(catalogItem);
+
+        ProvisionActionParameter actionParam = ProvisionActionParameterMother.of("mandatoryParam", (Object) null);
+        ProvisionAction action = ProvisionActionMother.of(List.of(actionParam));
+
+        // when / then
+        assertThatThrownBy(() -> validator.validate(action))
+                .isInstanceOf(InvalidRestEntityException.class)
+                .hasMessageContaining("mandatory and no value was provided");
+    }
+
+    @Test
+    void givenMandatoryParameterWithBlankStringValue_whenValidate_thenExceptionIsThrown() {
+        // given
+        var bearerToken = "bearer-token";
+
+        CatalogItem catalogItem = CatalogItemMother.of();
+        CatalogItemUserActionParameter mandatoryParam = CatalogItemUserActionParameterMother.of(
+                "mandatoryParam",
+                "defaultValue"
+        );
+        catalogItem.getUserActions().getFirst().setParameters(List.of(mandatoryParam));
+
+        when(authenticationProvider.getAccessToken()).thenReturn(bearerToken);
+        when(componentCatalogService.getCatalogItem(any(), any(), any()))
+                .thenReturn(catalogItem);
+
+        ProvisionActionParameter actionParam = ProvisionActionParameterMother.of("mandatoryParam", "   ");
+        ProvisionAction action = ProvisionActionMother.of(List.of(actionParam));
+
+        // when / then
+        assertThatThrownBy(() -> validator.validate(action))
+                .isInstanceOf(InvalidRestEntityException.class)
+                .hasMessageContaining("mandatory and no value was provided");
+    }
+
+    @Test
+    void givenMandatoryParameterWithNonBlankStringValue_whenValidate_thenValidationPasses() {
+        // given
+        var bearerToken = "bearer-token";
+
+        CatalogItem catalogItem = CatalogItemMother.of();
+        CatalogItemUserActionParameter mandatoryParam = CatalogItemUserActionParameterMother.of(
+                "mandatoryParam",
+                "defaultValue"
+        );
+        catalogItem.getUserActions().getFirst().setParameters(List.of(mandatoryParam));
+
+        when(authenticationProvider.getAccessToken()).thenReturn(bearerToken);
+        when(componentCatalogService.getCatalogItem(any(), any(), any()))
+                .thenReturn(catalogItem);
+
+        ProvisionActionParameter actionParam = ProvisionActionParameterMother.of("mandatoryParam", "someValue");
+        ProvisionAction action = ProvisionActionMother.of(List.of(actionParam));
+
+        // when / then (no exception)
+        validator.validate(action);
+    }
 }
