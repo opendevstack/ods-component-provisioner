7d1d0e0a7e191c8268fd4c167d0c918c7652e52e
Vila,Jordi (IT EDP) <jordi.vila@boehringer-ingelheim.com>
2026-05-06 11:30:29 +0200

    Change 200 to 204 as a delete return status

--- src/main/java/org/opendevstack/component_provisioner/server/controllers/ProvisionResultsApiController.java
+++ src/main/java/org/opendevstack/component_provisioner/server/controllers/ProvisionResultsApiController.java
@@ -66,7 +66,7 @@ public class ProvisionResultsApiController implements ProvisionResultsApi {
 
         provisionResultsApiFacade.deleteProvisioningStatus(projectKey, provisioningDeleteRequest.getComponentId());
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.noContent().build();
     }
 
     @Override
