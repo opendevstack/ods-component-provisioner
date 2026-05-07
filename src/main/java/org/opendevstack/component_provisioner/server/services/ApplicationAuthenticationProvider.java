c0421f8be92fa365f1785d433d04233f37584add
soriaoli <sergio.soria_olivares.ext@boehringer-ingelheim.com>
2026-05-07 14:55:48 +0200

    [component-registration-auth] - Generate application access token using app credentials.

--- src/main/java/org/opendevstack/component_provisioner/server/services/ApplicationAuthenticationProvider.java
+++ src/main/java/org/opendevstack/component_provisioner/server/services/ApplicationAuthenticationProvider.java
@@ -1,16 +1,33 @@
 package org.opendevstack.component_provisioner.server.services;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class ApplicationAuthenticationProvider {
 
+    private final AzureAdTokenService azureAdTokenService;
+
+    private final String clientId;
+    private final String clientSecret;
+    private final String scope;
+
+    public ApplicationAuthenticationProvider(AzureAdTokenService azureAdTokenService,
+                                             @Value("${component-provisioner.ods-api-service.params.client_id}") String clientId,
+                                             @Value("${component-provisioner.ods-api-service.params.client_secret}") String clientSecret,
+                                             @Value("${component-provisioner.ods-api-service.params.scope}") String scope) {
+        this.azureAdTokenService = azureAdTokenService;
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+        this.scope = scope;
+    }
+
     public String getAccessToken() {
         log.debug("Generating Auth token for application");
 
-        var accessToken = "To be properly calculated";
+        var accessToken = azureAdTokenService.getAccessToken(clientId, clientSecret, scope);
 
         log.debug("Extracted accessToken: {} from request.", accessToken);
 
---
+++ src/test/java/org/opendevstack/component_provisioner/server/services/ApplicationAuthenticationProviderTest.java
@@ -0,0 +1,43 @@
+package org.opendevstack.component_provisioner.server.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ApplicationAuthenticationProviderTest {
+
+    @Mock
+    private AzureAdTokenService azureAdTokenService;
+
+    private ApplicationAuthenticationProvider applicationAuthenticationProvider;
+
+    @BeforeEach
+    void setUp() {
+        applicationAuthenticationProvider = new ApplicationAuthenticationProvider(
+                azureAdTokenService,
+                "testClientId",
+                "testClientSecret",
+                "testScope"
+        );
+    }
+
+    @Test
+    void givenValidCredentials_whenGetAccessToken_thenReturnToken() {
+        // given
+        String expectedToken = "expectedAccessToken";
+        when(azureAdTokenService.getAccessToken("testClientId", "testClientSecret", "testScope"))
+                .thenReturn(expectedToken);
+
+        // when
+        String actualToken = applicationAuthenticationProvider.getAccessToken();
+
+        // then
+        assertThat(actualToken).isEqualTo(expectedToken);
+    }
+}
